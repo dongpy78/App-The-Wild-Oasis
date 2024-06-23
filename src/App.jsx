1da443f852d75683395e4ddc2284f8ca6323aef6
Van Dong <vandong78py@gmail.com>
2024-06-23 15:21:07 +0700

    Styled Component Props and css Function

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+
+const StyleApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyleApp>
+        <Heading as="h1">The Will Oasid</Heading>
+
+        <Heading as="h2">Check in and out</Heading>
+        <Button onClick={() => alert("Check in")}>Check in</Button>
+        <Button onClick={() => alert("Check out")}>Check out</Button>
+
+        <Heading as="h3">Forms</Heading>
+        <Input type="number" placeholder="Number of guest" />
+        <Input type="number" placeholder="Number of guest" />
+      </StyleApp>
+    </>
+  );
 }
 
 export default App;
