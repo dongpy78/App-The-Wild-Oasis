cf7f8079208f11e8f6f5c49cc65af8cdaf0ab12f
Van Dong <vandong78py@gmail.com>
2024-06-24 16:14:36 +0700

    Building the AppLayout

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Account from "./pages/Account";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import GlobalStyles from "./styles/GlobalStyles";
+import AppLayout from "./ui/AppLayout";
 
 function App() {
   return (
@@ -15,14 +16,18 @@ function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          {/* Trang Dashboard ta muốn thấy đầu tiên khi mở ứng dụng */}
-          <Route index element={<Navigate replace to="dashboard" />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="bookings" element={<Bookings />} />
-          <Route path="cabins" element={<Cabins />} />
-          <Route path="users" element={<Users />} />
-          <Route path="settings" element={<Settings />} />
-          <Route path="account" element={<Account />} />
+          <Route element={<AppLayout />}>
+            {/* Trang Dashboard ta muốn thấy đầu tiên khi mở ứng dụng */}
+            {/* Những Route con bên trong <AppLayout/> thì sẽ hiển thị trên giao diện <Applayout/> */}
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
+          </Route>
+          {/* Còn Router login ở bên ngoài thì sẽ hiển thị 1 trang riêng biệt */}
           <Route path="login" element={<Login />} />
           {/* Tuyến đường cuối cùng này sẽ được khớp nếu không có URL nào ở trên có thể khớp */}
           <Route path="*" element={<PageNotFound />} />
