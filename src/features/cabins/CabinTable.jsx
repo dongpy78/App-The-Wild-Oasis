223b7addde91a1577ae7a8edcab250af07498e63
Van Dong <vandong78py@gmail.com>
2024-10-12 20:56:11 +0700

    push code

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -6,6 +6,7 @@ import { useCabins } from "./useCabins";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
 import { useSearchParams } from "react-router-dom";
+import Empty from "../../ui/Empty";
 
 const TableHeader = styled.header`
   display: grid;
@@ -26,6 +27,8 @@ function CabinTable() {
   const { isLoading, cabins } = useCabins();
   const [searchParams] = useSearchParams();
   if (isLoading) return <Spinner />;
+  if (!cabins.length) return <Empty resourceName="cabins" />;
+
   // 1) FILTER
   const filterValue = searchParams.get("discount") || "all";
   let filteredCabins;
